c31b4f443fe8d3c0e147682b0c39c26fd1577b39
Satishs7 <Satish@s7works.io>
2020-09-30 20:43:27 +0530

    convert list to tuple

--- practise/test.py
+++ practise/test.py
@@ -1,5 +1,13 @@
 # How to convert a list into a string?
 
+# weekday=['mon','tue','wed','thurs','fri','sat','sun']
+# newstring =' '.join(weekday)
+# print(newstring)
+
+
+# How to convert a list into a tuple?
+
 weekday=['mon','tue','wed','thurs','fri','sat','sun']
-newstring =' '.join(weekday)
-print(newstring)
+tupweekday =tuple(weekday)
+print(tupweekday)
+print(type(tupweekday))
