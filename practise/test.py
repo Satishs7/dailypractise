e0e073fc52ad5ed9dc6949ad65a8604a8616a7da
Satishs7 <Satish@s7works.io>
2020-09-30 20:45:36 +0530

    convert list to set

--- practise/test.py
+++ practise/test.py
@@ -7,7 +7,12 @@
 
 # How to convert a list into a tuple?
 
-weekday=['mon','tue','wed','thurs','fri','sat','sun']
-tupweekday =tuple(weekday)
-print(tupweekday)
-print(type(tupweekday))
+# weekday=['mon','tue','wed','thurs','fri','sat','sun']
+# tupweekday =tuple(weekday)
+# print(tupweekday)
+
+# How to convert a list into a set
+
+# weekday=['mon','tue','wed','thurs','fri','sat','sun','sun']
+# setweekday =set(weekday)
+# print(setweekday)
