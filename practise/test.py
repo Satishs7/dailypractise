bc6729d94a23269943e57349dea163b10c17fe18
Satishs7 <Satish@s7works.io>
2020-09-30 20:51:08 +0530

    count element

--- practise/test.py
+++ practise/test.py
@@ -16,3 +16,11 @@
 # weekday=['mon','tue','wed','thurs','fri','sat','sun','sun']
 # setweekday =set(weekday)
 # print(setweekday)
+
+
+# How to count the occurrences of a particular element in the list?
+# weekday=['mon','tue','wed','thurs','fri','sat','sun','thurs','fri','sat','thurs','fri','sat','sun','thurs','fri','sat','thurs','fri','sat','sun','thurs','fri','sat',]
+#
+# print(weekday.count('fri'))
+#
+# print(weekday.count('thurs'))
